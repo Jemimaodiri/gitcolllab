c4f3d4bc76d4987f0839eff5a4c7d7bf39f7ed65
zionakwubo01 <zionakwubo@gmail.com>
2023-11-22 11:35:53 +0100

    third commit

--- Controller.ts
+++ Controller.ts
@@ -1,1 +1,25 @@
 import { Request, Response } from "express";
+
+
+const CreateTask = async (req: Request, res: Response) => {
+    try {
+        const { taskName, Deadline, done } = req.body
+
+        const taskModel = await Taskmodel.create({
+            taskName, Deadline, done
+
+        })
+        return res.status(200).json({
+            message: "task created",
+            data: taskModel
+        })
+
+
+
+    } catch (error) {
+
+        return res.status(404).json({
+            message: "error"
+        })
+    }
+}
